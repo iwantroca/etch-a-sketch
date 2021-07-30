5460d2376a5842a5dc4f5d03557cb79b9273eec3
Iwantroca <mohitnotiyal@yahoo.com>
2021-07-30 21:19:25 +0530

    Add working to color button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("#canvas");
 const resetBtn = document.querySelector("#reset-btn");
+const colorSetter = document.querySelector("#color-setter");
 let divList = [];
+let newColor;
+
 let gridNum = parseInt(prompt("Enter the grid rows"));
 canvas.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
 canvas.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
@@ -12,22 +15,27 @@ function createGrid() {
     divList.push(div);
   }
 }
+function colorChanger() {
+  newColor = colorSetter.value;
+  return newColor;
+}
 
 createGrid();
 divList.forEach((div) => canvas.appendChild(div));
 
 const gridItems = document.querySelectorAll(".grid-items");
 
+colorSetter.addEventListener("mouseleave", (e) => colorChanger());
+
 gridItems.forEach((item) =>
   item.addEventListener(
     "mouseenter",
-    (e) => (e.target.style.background = "black")
+    (e) => (e.target.style.background = `${newColor}`)
   )
 );
 function resetCanvas() {
   divList.forEach((div) => (div.style.backgroundColor = "white"));
 }
-
 resetBtn.addEventListener("click", () => {
   resetCanvas();
 });
