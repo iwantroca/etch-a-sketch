fe45d2cad5e8a9a920acd3303f73c8c5fc7d2453
Iwantroca <mohitnotiyal@yahoo.com>
2021-07-30 15:30:26 +0530

    Fix grid-items sizing

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const canvas = document.querySelector("#canvas");
 let divList = [];
-// create divs;
-// take input and form that many divs
+// create divs; X
+// take input and form that many divs X
+// add correct size to each div to keep them responsive
 // add coloring methods
-for (let i = 0; i < 1350; i++) {
+
+let gridNum = parseInt(prompt("Enter the grid rows"));
+canvas.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+canvas.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+
+for (let i = 0; i < gridNum * gridNum; i++) {
   let div = document.createElement("div");
-  div.classList.add("items");
-  div.textContent = "1";
+  div.classList.add("grid-items");
   divList.push(div);
 }
 divList.forEach((div) => canvas.appendChild(div));
