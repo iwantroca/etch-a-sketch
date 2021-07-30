e445ea1d9d76dd3a92e219daa82796049f30e34a
Iwantroca <mohitnotiyal@yahoo.com>
2021-07-30 18:33:10 +0530

    Add working to reset button

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const canvas = document.querySelector("#canvas");
+const resetBtn = document.querySelector("#reset-btn");
 let divList = [];
 let gridNum = parseInt(prompt("Enter the grid rows"));
 canvas.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
 canvas.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
 
-for (let i = 0; i < gridNum * gridNum; i++) {
-  let div = document.createElement("div");
-  div.classList.add("grid-items");
-  divList.push(div);
+function createGrid() {
+  for (let i = 0; i < gridNum * gridNum; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid-items");
+    divList.push(div);
+  }
 }
 
+createGrid();
 divList.forEach((div) => canvas.appendChild(div));
+
 const gridItems = document.querySelectorAll(".grid-items");
 
 gridItems.forEach((item) =>
@@ -19,3 +24,10 @@ gridItems.forEach((item) =>
     (e) => (e.target.style.background = "black")
   )
 );
+function resetCanvas() {
+  divList.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+resetBtn.addEventListener("click", () => {
+  resetCanvas();
+});
