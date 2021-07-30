31982882f8bb6c17434bde5753dc774abe9a6516
Iwantroca <mohitnotiyal@yahoo.com>
2021-07-30 18:21:26 +0530

    Make sketching work

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const canvas = document.querySelector("#canvas");
 let divList = [];
-// create divs; X
-// take input and form that many divs X
-// add correct size to each div to keep them responsive
-// add coloring methods
-
 let gridNum = parseInt(prompt("Enter the grid rows"));
 canvas.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
 canvas.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
@@ -14,4 +9,13 @@ for (let i = 0; i < gridNum * gridNum; i++) {
   div.classList.add("grid-items");
   divList.push(div);
 }
+
 divList.forEach((div) => canvas.appendChild(div));
+const gridItems = document.querySelectorAll(".grid-items");
+
+gridItems.forEach((item) =>
+  item.addEventListener(
+    "mouseenter",
+    (e) => (e.target.style.background = "black")
+  )
+);
